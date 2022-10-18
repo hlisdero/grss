e73ae1ee1827790708e6d3c88b6820a5be1e954b
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-10-18 20:53:34 +0200

    Use writeln! instead of println!

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use clap::Parser;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::io::{self, Write};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -14,6 +15,8 @@ struct Cli {
 }
 
 fn main() -> Result<()> {
+    let stdout = io::stdout(); // get the global stdout entity
+    let mut stdout_handle = stdout.lock(); // acquire a lock on it
     let args = Cli::parse();
     let file = File::open(&args.path)
         .with_context(|| format!("could not open file `{}`", &args.path.to_string_lossy()))?;
@@ -28,8 +31,11 @@ fn main() -> Result<()> {
             break;
         }
         if line.contains(&args.pattern) {
-            print!("{}", line);
+            writeln!(stdout_handle, "{}", line).with_context(|| "could not write to stdout")?;
         }
     }
+    stdout_handle
+        .flush()
+        .with_context(|| "could not flush output to stdout")?;
     Ok(())
 }
