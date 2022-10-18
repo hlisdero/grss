7b6cc79b7c74e8e1f9e31a68a0b809d224be0606
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-10-18 21:06:05 +0200

    Added logger

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::{Context, Result};
 use clap::Parser;
+use log::{info, warn};
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -15,21 +16,26 @@ struct Cli {
 }
 
 fn main() -> Result<()> {
+    env_logger::init();
     let stdout = io::stdout(); // get the global stdout entity
     let mut stdout_handle = stdout.lock(); // acquire a lock on it
+    info!("Parsing arguments");
     let args = Cli::parse();
+    info!("Opening file");
     let file = File::open(&args.path)
         .with_context(|| format!("could not open file `{}`", &args.path.to_string_lossy()))?;
     let mut reader = BufReader::new(file);
 
     loop {
         let mut line = String::new();
+        info!("Reading line from file");
         let len = reader
             .read_line(&mut line)
             .with_context(|| format!("could not read file `{}`", &args.path.to_string_lossy()))?;
         if len <= 0 {
             break;
         }
+        info!("Checking if line contains the pattern");
         if line.contains(&args.pattern) {
             writeln!(stdout_handle, "{}", line).with_context(|| "could not write to stdout")?;
         }
