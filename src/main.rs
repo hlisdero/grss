9d0855cda32ff731213685e14d6495c378077026
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2022-10-18 20:28:04 +0200

    1.3 First implementation (exercise completed)

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use clap::Parser;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+    let file = File::open(&args.path).expect("could not read file");
+    let mut reader = BufReader::new(file);
+
+    loop {
+        let mut line = String::new();
+        let len = reader
+            .read_line(&mut line)
+            .expect("error when reading line");
+        if len <= 0 {
+            break;
+        }
+        if line.contains(&args.pattern) {
+            print!("{}", line);
+        }
+    }
+}
